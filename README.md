55c65f7a6d7cd5c22cac093101123314350872ec
thanh1612 <thanhvn96@gmail.com:git config --global user.email thanhvn96@gmail.comgit config --global user.email thanhvn96@gmail.com>
2014-10-17 02:50:59 +0700

    Projet game

--- README.md
+++ README.md
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-TEST modification	  
-
-=======
 Test modification OL.
->>>>>>> 96723b187e06320a8b25d2abfbcf99c292170b3e
 
 NguyenCongThanhCasseBrique
 ==========================
---
+++ projet.cc
@@ -0,0 +1,169 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <unistd.h>
+#include <termios.h>
+#include <fcntl.h>
+using namespace std;
+
+int read_keybord();
+
+/******************************************************************************/
+
+#define H 20
+#define L 50
+
+char screen[H][L];
+
+void display()
+{
+  system("clear"); 
+  for (int i=0; i<H; i++)
+    {
+      for (int j=0; j<L; j++)
+	if (i==0||i==H-1||j==0||j==L-1) screen[i][j]='*';
+      cout<< endl;
+    }
+  
+  for (int i=0;i<H;i++)
+    {
+      for (int j=0; j<L; j++)
+	cout << screen[i][j];
+      cout << endl;
+    }
+}
+
+/******************************************************************************/
+
+void clear_screen()
+{
+  for (int i=1; i<H-1; i++)
+    for (int j=1; j<L-1; j++)
+      screen[i][j] = ' ';
+}			  
+
+/*****************************************************************************/
+
+int x=1; int y=1;
+int oppo=1;
+int trans=1;
+
+void update_game(int key)
+{
+  if ( oppo==1 && trans==1 && (x>1 || y>1)) 
+   {
+     screen[x][y]='o';
+     x++;
+     y++;
+   }
+  if (x==H-1) 
+    {
+      trans=0;
+      x--;
+      y--;
+    } 
+  if (oppo==1 && trans==0)
+    {
+      if (x>1 && y<L-1)
+	{
+	  screen[x][y]='o';
+	  x--;
+	  y++;
+	}
+      else trans=1;
+    }
+  if (y==L-1) 
+    {
+      oppo=0;
+      x--;
+      y--;
+    }    
+  
+  /***************************************************/
+
+  if ( oppo==0 && trans==1 && (x>1 || y<L-1))
+    {
+      screen[x][y]='o';
+      x++;
+      y--;
+    }
+  if (x==H-1)
+    {
+      trans=0;
+      x--;
+      y++;    
+    }
+  if ( oppo==0 && trans==0)
+    {
+      if (x>1 && y>1)
+	{
+	  screen[x][y]='o';
+	  x--;
+	  y--;
+	}
+      else trans=1;
+    }     
+  if (y==1)
+    {
+      oppo = 1;
+      x++;
+      y++;
+    }
+}
+	
+/*****************************************************************************/
+
+int main()
+{
+  int key;
+  do
+    {
+      key=read_keybord();			
+      clear_screen();
+      update_game(key);
+      display();
+      usleep(50*1000);
+    }
+  while (key!='q');
+}
+
+/*****************************************************************************/
+
+int kbhit(void)
+{
+  struct termios oldt, newt;
+  int ch;
+  int oldf;
+
+  tcgetattr(STDIN_FILENO, &oldt);
+  newt = oldt;
+  newt.c_lflag &= ~(ICANON | ECHO);
+  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+  oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+  fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+
+  ch = getchar();
+
+  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+  fcntl(STDIN_FILENO, F_SETFL, oldf);
+
+  if(ch != EOF)
+  {
+    ungetc(ch, stdin);
+    return 1;
+  }
+
+  return 0;
+}
+
+int read_keybord() 
+{
+  int key;
+  if (kbhit())
+    key = getc(stdin);
+  else
+    key = 0;
+  return key;
+}
+
+/******************************************************************************/
